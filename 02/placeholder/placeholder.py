d7e254de2da3e847ad05f8b19f3ac50d95b91dc9
Anton Nikulin <dr.tallin@gmail.com>
2015-01-23 23:04:52 +0200

    server side caching

--- 02/placeholder/placeholder.py
+++ 02/placeholder/placeholder.py
@@ -26,6 +26,7 @@ import hashlib
 import os
 from django import forms
 from django.conf.urls import url
+from django.core.cache import cache
 from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.http import etag
@@ -40,17 +41,21 @@ class ImageForm(forms.Form):
     def generate(self, image_format='PNG'):
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
-        image = Image.new('RGB', (width, height))
-        draw = ImageDraw.Draw(image)
-        text = '{} X {}'.format(width, height)
-        textwidth, textheight = draw.textsize(text)
-        if textwidth < width and textheight < height:
-            texttop = (height - textheight) // 2
-            textleft = (width - textwidth) // 2
-            draw.text((textleft, texttop), text, fill=(255, 255, 255))
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} X {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(255, 255, 255))
         content = BytesIO()
         image.save(content, image_format)
         content.seek(0)
+        cache.set(key, content, 60*60)
         return content
 
 def generate_etag(request, width, height):
